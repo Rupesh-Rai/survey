c6db6393048f2dcebc6665a0613d868ad836224d
Sahil Khadka <sahilkhadka767@gmail.com>
2022-04-20 18:06:54 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./app.css"
 function App() {
+  const [details, setDetails] = useState({
+    fname: "",
+    phone: "",
+    message: ""
+  })
+  const handleChange = (event) => {
+    const {name, value} = event.target
+    setDetails((prevState) => {
+      return (
+        {
+          ...prevState,
+          [name] : value
+        }
+      )
+    })
+  }
+  const handleButton = (event) => {
+    event.preventDefault()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Survey Form</h1>
+      <form action="">
+        <label htmlFor="fname">Name:</label>
+        <input type="text" name="fname" id = "fname" value={details.fname} onChange = {handleChange} required />
+        <label htmlFor="phone-num">Phone number:</label>
+        <input type= "tel" name="phone" id="phone-num" value = {details.phone} onChange = {handleChange} required />
+        <label htmlFor="message">Message:</label>
+        <textarea name="message" id="message" value={details.message} onChange = {handleChange}></textarea>
+        <button type="submit" onClick={handleButton}>Submit</button>
+      </form>
+    </>
   );
 }
-
 export default App;
